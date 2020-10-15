032d52e0a239ff6ece28cc5c2f8bddabc348d37c
Mara Mulato <mara.mulato@gmail.com>
2020-10-15 18:37:44 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,29 @@
+let selector = 0
+input.onGesture(Gesture.Shake, function () {
+    selector = randint(1, 3)
+    if (selector == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (selector == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
